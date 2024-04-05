d21d8379aa4991db75159a35fec213dc20943de1
qusr08 <34079525+qusr08@users.noreply.github.com>
2024-04-05 02:13:02 -0400

    Added blank files for future code

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+	List<TileGroup> tileGroups = new List<TileGroup>( );
+
+	private void GenerateTileGroups (int tileCount) {
+
+	}
+}
