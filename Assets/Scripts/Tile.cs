02f2a347b9e983990c4fc0620f97a9bf24067695
qusr08 <34079525+qusr08@users.noreply.github.com>
2024-04-05 16:56:39 -0400

    Added isometric tile positioning

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Tile : MonoBehaviour {
+	[Header("References")]
+	[SerializeField] private SpriteRenderer spriteRenderer;
+	[Header("Properties")]
+	[SerializeField] private Vector2Int position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void OnValidate ( ) {
+		UpdateIsometricPosition( );
+	}
+
+	private void Awake ( ) {
+		OnValidate( );
+	}
+
+	private void Update ( ) {
+		UpdateIsometricPosition( );
+	}
+
+	private void UpdateIsometricPosition ( ) {
+		// Make sure tiles always align to the isometric grid
+		transform.position = new Vector2((position.x + position.y) * 0.5f, (position.y - position.x) * 0.25f);
+
+		// Make sure tiles that have a lower y position appear in front of others
+		spriteRenderer.sortingOrder = position.x - position.y;
+	}
 }
