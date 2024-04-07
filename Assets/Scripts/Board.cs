b3fc087451b698d576c6dd20ab851c96b163400f
qusr08 <34079525+qusr08@users.noreply.github.com>
2024-04-06 23:14:25 -0400

    Added comments and started generating tile groups

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -9,13 +9,14 @@ public class Board : Singleton<Board> {
 	[SerializeField] private GameObject tilePrefab;
 	[Header("Properties")]
 	[SerializeField, Min(1)] private int tileCount;
+	[SerializeField, Min(1)] private int tileGroupCount;
 
 	private List<List<Tile>> tiles = new List<List<Tile>>( );
 
 	protected override void Awake ( ) {
 		base.Awake( );
 
-		// Create the list of tiles on the board
+		// Declare the list of tile groups
 		tiles = new List<List<Tile>>( );
 	}
 
@@ -23,7 +24,10 @@ public class Board : Singleton<Board> {
 		Generate( );
 	}
 
-	private void Generate ( ) {
+	/// <summary>
+	/// Clear all the tiles that are currently on the board
+	/// </summary>
+	private void Clear ( ) {
 		// Destroy all tiles that are already in the list
 		while (tiles.Count > 0) {
 			while (tiles[0].Count > 0) {
@@ -33,19 +37,25 @@ public class Board : Singleton<Board> {
 
 			tiles.RemoveAt(0);
 		}
+	}
 
+	/// <summary>
+	/// Generate all of the tiles and tile groups that will be on the board
+	/// </summary>
+	private void Generate ( ) {
 		// Lists that store what positions on the board have already been taken or are available to spawn a tile at
 		List<Vector2Int> availablePositions = new List<Vector2Int>( ) { Vector2Int.zero };
 		List<Vector2Int> takenPositions = new List<Vector2Int>( );
+		List<Tile> createdTiles = new List<Tile>( );
 
 		// Generate all the tiles
 		for (int i = 0; i < tileCount; i++) {
 			// Get a random available position from the list
 			Vector2Int randomBoardPosition = availablePositions[Random.Range(0, availablePositions.Count)];
+			availablePositions.Remove(randomBoardPosition);
 
 			// Create a new tile at that position
-			CreateTile(randomBoardPosition);
-			availablePositions.Remove(randomBoardPosition);
+			createdTiles.Add(CreateTile(randomBoardPosition));
 			takenPositions.Add(randomBoardPosition);
 
 			// Calculate the four cardinal direction vectors from the newly chosen board position
@@ -69,8 +79,27 @@ public class Board : Singleton<Board> {
 				availablePositions.Add(leftPosition);
 			}
 		}
+
+		// Select random tiles on the board that will be starting points for the tile groups
+		for (int i = 0; i < tileGroupCount; i++) {
+			// Find a random tile on the board
+			Tile randomTile = createdTiles[Random.Range(0, createdTiles.Count)];
+			createdTiles.Remove(randomTile);
+
+			// Add the tile to its tile group
+			tiles.Add(new List<Tile>( ) { randomTile });
+			randomTile.TileGroupID = i;
+
+			// TEST: change the color of the tile to a random color to show the different tile groups
+			randomTile.GetComponent<SpriteRenderer>( ).color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+		}
 	}
 
+	/// <summary>
+	/// Get the tile at the specified board position
+	/// </summary>
+	/// <param name="boardPosition">The board position to check for a tile at</param>
+	/// <returns>A reference to the tile if there is one at the inputted board position, null otherwise</returns>
 	private Tile GetTile (Vector2Int boardPosition) {
 		// The raycast origin will be right above the tile on the z axis
 		Vector3 raycastOrigin = BoardPositionToWorldPosition(boardPosition) + Vector3.back;
@@ -83,14 +112,28 @@ public class Board : Singleton<Board> {
 		return null;
 	}
 
+	/// <summary>
+	/// Create a new tile on the board
+	/// </summary>
+	/// <param name="boardPosition">The board position to initially set the tile to</param>
+	/// <param name="tileGroupID">The ID of the tile group for this block</param>
+	/// <returns>A reference to the newly created tile</returns>
 	public Tile CreateTile (Vector2Int boardPosition, int tileGroupID = -1) {
-		Tile tile = Instantiate(tilePrefab, (Vector2) boardPosition, Quaternion.identity, transform).GetComponent<Tile>( );
+		// Create the tile object in the scene
+		Tile tile = Instantiate(tilePrefab, Vector3.zero, Quaternion.identity, transform).GetComponent<Tile>( );
+		
+		// Set the tile variables
 		tile.BoardPosition = boardPosition;
 		tile.TileGroupID = tileGroupID;
 
 		return tile;
 	}
 
+	/// <summary>
+	/// Convert a board position to a world position
+	/// </summary>
+	/// <param name="boardPosition">The 2D board position to convert</param>
+	/// <returns>A Vector3 that is the world position equivelant to the inputted board position</returns>
 	public static Vector3 BoardPositionToWorldPosition (Vector2Int boardPosition) {
 		return new Vector3((boardPosition.x + boardPosition.y) * 0.5f, (boardPosition.y - boardPosition.x) * 0.25f);
 	}
