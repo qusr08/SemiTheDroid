da92780d8909215c0fb791617f9b0022d3fff68c
qusr08 <34079525+qusr08@users.noreply.github.com>
2024-04-07 00:30:27 -0400

    Added more of tile group gen algorithm

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -48,7 +48,9 @@ public class Board : Singleton<Board> {
 		// Lists that store what positions on the board have already been taken or are available to spawn a tile at
 		List<Vector2Int> availablePositions = new List<Vector2Int>( ) { Vector2Int.zero };
 		List<Vector2Int> takenPositions = new List<Vector2Int>( );
-		List<Tile> createdTiles = new List<Tile>( );
+
+		// A list containing all of the tiles that have been created and are unassigned to a tile group
+		List<Tile> unassignedTiles = new List<Tile>( );
 
 		// Generate all the tiles
 		for (int i = 0; i < tileCount; i++) {
@@ -57,28 +59,15 @@ public class Board : Singleton<Board> {
 			availablePositions.Remove(randomBoardPosition);
 
 			// Create a new tile at that position
-			createdTiles.Add(CreateTile(randomBoardPosition));
+			unassignedTiles.Add(CreateTile(randomBoardPosition));
 			takenPositions.Add(randomBoardPosition);
 
-			// Calculate the four cardinal direction vectors from the newly chosen board position
-			Vector2Int upPosition = randomBoardPosition + Vector2Int.up;
-			Vector2Int rightPosition = randomBoardPosition + Vector2Int.right;
-			Vector2Int downPosition = randomBoardPosition + Vector2Int.down;
-			Vector2Int leftPosition = randomBoardPosition + Vector2Int.left;
-
 			// Add all surrounding tile positions to the available tile positions
-			// Do not add the position if it has already been taken by a tile though OR if it has already been added
-			if (!takenPositions.Contains(upPosition) && !availablePositions.Contains(upPosition)) {
-				availablePositions.Add(upPosition);
-			}
-			if (!takenPositions.Contains(rightPosition) && !availablePositions.Contains(rightPosition)) {
-				availablePositions.Add(rightPosition);
-			}
-			if (!takenPositions.Contains(downPosition) && !availablePositions.Contains(downPosition)) {
-				availablePositions.Add(downPosition);
-			}
-			if (!takenPositions.Contains(leftPosition) && !availablePositions.Contains(leftPosition)) {
-				availablePositions.Add(leftPosition);
+			foreach (Vector2Int cardinalPosition in GetCardinalBoardPositions(randomBoardPosition)) {
+				// Do not add the position if it has already been taken by a tile though OR if it has already been added
+				if (!takenPositions.Contains(cardinalPosition) && !availablePositions.Contains(cardinalPosition)) {
+					availablePositions.Add(cardinalPosition);
+				}
 			}
 		}
 
@@ -101,15 +90,15 @@ public class Board : Singleton<Board> {
 		// The number of groups that have currently been created
 		int groupCount = 0;
 
-		while (createdTiles.Count > 0) {
+		while (unassignedTiles.Count > 0) {
 			// Generate a random size for the current tile group
 			int groupTileCount = Random.Range(minGroupTileCount, maxGroupTileCount + 1);
 
 			// Find a random tile on the board to start the tile group
-			Tile randomTile = createdTiles[Random.Range(0, createdTiles.Count)];
-			createdTiles.Remove(randomTile);
+			Tile randomTile = unassignedTiles[Random.Range(0, unassignedTiles.Count)];
+			unassignedTiles.Remove(randomTile);
 
-			for (int i = 0; i < groupTileCount; i++) {
+			for (int i = 1; i < groupTileCount; i++) {
 				// Add the tile to its tile group
 				tiles.Add(new List<Tile>( ) { randomTile });
 				randomTile.TileGroupID = groupCount;
@@ -123,17 +112,53 @@ public class Board : Singleton<Board> {
 	/// Get the tile at the specified board position
 	/// </summary>
 	/// <param name="boardPosition">The board position to check for a tile at</param>
+	/// <param name="tileGroupID">The ID that the tile must be in order for this function to return a tile object. Having this value be -1 ignores this feature</param>
 	/// <returns>A reference to the tile if there is one at the inputted board position, null otherwise</returns>
-	private Tile GetTile (Vector2Int boardPosition) {
+	private Tile GetTile (Vector2Int boardPosition, int tileGroupID = -1) {
 		// The raycast origin will be right above the tile on the z axis
 		Vector3 raycastOrigin = BoardPositionToWorldPosition(boardPosition) + Vector3.back;
 
-		// Fire a raycast in the direction of the tiles. If it hits a tile, return it
+		// Fire a raycast in the direction of the tiles to see if it hits one
 		if (Physics.Raycast(raycastOrigin, Vector3.forward, out RaycastHit hit)) {
-			return hit.transform.GetComponent<Tile>( );
+			// Get a reference to the tile that was hit by the raycast
+			Tile hitTile = hit.transform.GetComponent<Tile>( );
+
+			// Only return the tile if it has a matching tile group ID
+			if ((tileGroupID != -1 && hitTile != null && hitTile.TileGroupID == tileGroupID) || tileGroupID == -1) {
+				return hitTile;
+			}
 		}
 
 		return null;
+	}
+
+	/// <summary>
+	/// Get all four cardinal tiles around the specified board position (if they exist)
+	/// </summary>
+	/// <param name="boardPosition">The board position to get the cardinal tiles around</param>
+	/// <param name="tileGroupID">The ID that the tile must be in order for this function to return a tile object. Having this value be -1 ignores this feature</param>
+	/// <returns>A list of all cardinal tiles around the specified board position. If an element is null, then that tile either doesn't exist or does not match the specified tile group ID</returns>
+	private List<Tile> GetCardinalTiles (Vector2Int boardPosition, int tileGroupID = -1) {
+		return new List<Tile>( ) {
+			GetTile(boardPosition + Vector2Int.up, tileGroupID: tileGroupID),
+			GetTile(boardPosition + Vector2Int.right, tileGroupID: tileGroupID),
+			GetTile(boardPosition + Vector2Int.down, tileGroupID: tileGroupID),
+			GetTile(boardPosition + Vector2Int.left, tileGroupID: tileGroupID)
+		};
+	}
+
+	/// <summary>
+	/// Get all four cardinal board positions around the specified board position
+	/// </summary>
+	/// <param name="boardPosition">The board position to get the cardinal board positions around</param>
+	/// <returns>A list of all the cardinal board positions around the specified board position</returns>
+	private List<Vector2Int> GetCardinalBoardPositions (Vector2Int boardPosition) {
+		return new List<Vector2Int>( ) {
+			boardPosition + Vector2Int.up,
+			boardPosition + Vector2Int.right,
+			boardPosition + Vector2Int.down,
+			boardPosition + Vector2Int.left
+		};
 	}
 
 	/// <summary>
@@ -145,7 +170,7 @@ public class Board : Singleton<Board> {
 	public Tile CreateTile (Vector2Int boardPosition, int tileGroupID = -1) {
 		// Create the tile object in the scene
 		Tile tile = Instantiate(tilePrefab, Vector3.zero, Quaternion.identity, transform).GetComponent<Tile>( );
-		
+
 		// Set the tile variables
 		tile.BoardPosition = boardPosition;
 		tile.TileGroupID = tileGroupID;
