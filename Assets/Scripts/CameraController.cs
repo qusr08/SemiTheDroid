1cda67ba9813ed661e0da09380813fb3b833785a
qusr08 <34079525+qusr08@users.noreply.github.com>
2024-04-11 00:36:32 -0400

    Improved camera zoom functionality

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,41 +11,50 @@ public class CameraController : MonoBehaviour {
 	[SerializeField, Min(0.01f)] private float minCameraZoom;
 	[SerializeField, Min(0.01f)] private float maxCameraZoom;
 	[SerializeField, Min(0.01f)] private float cameraZoomStep;
-	[SerializeField, Min(0)] private float cameraBorder;
+	[SerializeField, Min(0)] private float extensionPerZoomLevel;
 	[Space]
 	[SerializeField] private Vector3 panOrigin;
 
 	private void Update ( ) {
+		// Variables to store the current camera values
+		Vector3 cameraPosition = transform.position;
+		float cameraOrthographicSize = gameCamera.orthographicSize;
+
 		// When the middle mouse button is pressed, reset the last mouse position
 		if (Input.GetMouseButtonDown(2)) {
 			panOrigin = gameCamera.ScreenToWorldPoint(Input.mousePosition);
 		}
 
-		// If the player is pressing the middle mouse button, pan the camera around based on the mouse movement
-		if (Input.GetMouseButton(2)) {
-			// Calculate the movement of the mouse since the last frame
-			Vector3 panMotion = panOrigin - gameCamera.ScreenToWorldPoint(Input.mousePosition);
-			Vector3 newCameraPosition = transform.position + panMotion;
-
-			// Calculate the bounds of the screen
-			Vector2 screenBounds = new Vector2(
-				(gameCamera.orthographicSize * gameCamera.aspect) - cameraBorder,
-				gameCamera.orthographicSize - cameraBorder
-			);
-
-			// Clamp how far the camera position can get from the center of the board
-			Vector3 boardCenterDifference = (Vector3) BoardManager.Instance.CenterPosition - newCameraPosition;
-			boardCenterDifference.x = Mathf.Clamp(boardCenterDifference.x, -screenBounds.x, screenBounds.x);
-			boardCenterDifference.y = Mathf.Clamp(boardCenterDifference.y, -screenBounds.y, screenBounds.y);
-			newCameraPosition = (Vector3) BoardManager.Instance.CenterPosition - boardCenterDifference;
-
-			// Set the position of the camera
-			SetTransformPositionWithoutZ(transform, newCameraPosition);
-		}
-
 		// Zoom the camera in and out based on the scroll wheel value
 		gameCamera.orthographicSize -= Input.mouseScrollDelta.y * cameraZoomStep;
 		gameCamera.orthographicSize = Mathf.Clamp(gameCamera.orthographicSize, minCameraZoom, maxCameraZoom);
+
+		// If the player is pressing the middle mouse button, pan the camera around based on the mouse movement
+		if (Input.GetMouseButton(2)) {
+			// Calculate the movement of the mouse since the last frame
+			cameraPosition += panOrigin - gameCamera.ScreenToWorldPoint(Input.mousePosition);
+		}
+
+		// If something about the camera has changed, then update the position
+		if (cameraPosition != transform.position || cameraOrthographicSize != gameCamera.orthographicSize) {
+			// Get the current camera padding value that scales based on the current zoom level of the camera
+			float cameraExtension = (maxCameraZoom - gameCamera.orthographicSize) * extensionPerZoomLevel;
+
+			// Calculate the bounds of the screen
+			Vector2 screenBounds = new Vector2(
+				(gameCamera.orthographicSize * gameCamera.aspect) + cameraExtension,
+				gameCamera.orthographicSize + cameraExtension
+			);
+
+			// Clamp how far the camera position can get from the center of the board
+			Vector3 boardCenterDifference = (Vector3) BoardManager.Instance.CenterPosition - cameraPosition;
+			boardCenterDifference.x = Mathf.Clamp(boardCenterDifference.x, -screenBounds.x, screenBounds.x);
+			boardCenterDifference.y = Mathf.Clamp(boardCenterDifference.y, -screenBounds.y, screenBounds.y);
+			cameraPosition = (Vector3) BoardManager.Instance.CenterPosition - boardCenterDifference;
+
+			// Set the position of the camera
+			SetTransformPositionWithoutZ(transform, cameraPosition);
+		}
 	}
 
 	/// <summary>
